c370b01ac023283621e6a6331efde1a5be97f370
angarita.Dev <44899916+codingAngarita@users.noreply.github.com>
2020-07-27 15:07:33 -0500

    Changed resize method

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const config = {
   scale: {
     width: 800,
     height: 600,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.FIT;
   },
   physics: {
     default: 'arcade',
